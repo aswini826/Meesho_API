5289df19fd467f5bf7e70eeb471564888a4ac044
ASWINI A <113587646+aswini826@users.noreply.github.com>
2023-06-15 15:01:01 +0530

    Update models.py

--- demo/models.py
+++ demo/models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Users(Base):
@@ -10,6 +10,27 @@ class Users(Base):
     description = Column(String)
     price = Column(Integer)
     quantity = Column(Integer)
-    seller_id = Column(Integer)
+    seller_id = Column(Integer, unique=True)
 
 
+class Catagory(Base):
+    __tablename__ = 'catagory'
+
+    id = Column(Integer, primary_key=True, index=True)
+    catagory_id = Column(Integer, ForeignKey(Users.id))
+    name = Column(String, unique=True)
+    description = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+
+class Customer(Base):
+    __tablename__ = 'customer'
+
+    id = Column(Integer, primary_key=True, index=True)
+    customer_id = Column(Integer, ForeignKey(Catagory.id))
+    customer_name = Column(String)
+    email = Column(String, unique=True)
+    age = Column(Integer)
+    gender = Column(String)
+    phone = Column(String)
